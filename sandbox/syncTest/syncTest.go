b54415446b98b10967f2200dc2b8bff27240a90d
Your Name <you@example.com>
2024-03-05 18:14:06 +0300

    Filter with mutex

--- sandbox/syncTest/syncTest.go
+++ sandbox/syncTest/syncTest.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 )
 
+// Реализация функции Filter с использованием мьютекса
 func Filter[T any](s []T, predicate func(T) (ok bool)) (result []T) {
 	var wg sync.WaitGroup
-	output := make([]T, 0, len(s)) // Предварительное выделение памяти может ускорить работу
-	mux := &sync.Mutex{}           // Мьютекс для синхронизации доступа к output
+	result = make([]T, 0, len(s)) // Предварительное выделение памяти может ускорить работу
+	mux := &sync.Mutex{}          // Мьютекс для синхронизации доступа к output
 
 	for _, element := range s {
 		wg.Add(1)
 		go func(e T) {
 			defer wg.Done()
 			if predicate(e) {
-				mux.Lock() // Блокировка мьютекса
-				output = append(output, e)
+				mux.Lock() // Блокировка мьютекса для записи
+				result = append(result, e)
 				mux.Unlock() // Разблокировка мьютекса
 			}
 		}(element)
 	}
-
 	wg.Wait() // Ожидание завершения всех горутин
-	return output
+	return result
 }
 
 func main() {
@@ -34,5 +35,8 @@ func main() {
 	}
 
 	filtered := Filter(numbers, isEven)
+	sort.Slice(filtered, func(i, j int) bool {
+		return filtered[i] < filtered[j]
+	})
 	fmt.Println(filtered) // Вывод: 10 2 4 6 8
 }
--- slice/slice.go
+++ slice/slice.go
@@ -1,11 +1,27 @@
 package slice
 
+import "sync"
+
+// Реализация функции Filter с использованием мьютекса
 func Filter[T any](s []T, predicate func(T) (ok bool)) (result []T) {
-	output := []T{}
+	var wg sync.WaitGroup
+	result = make([]T, 0, len(s)) // Предварительное выделение памяти может ускорить работу
+	mux := &sync.Mutex{}          // Мьютекс для синхронизации доступа к output
+
+	// Для каждого элемента слайса запускаем горутину
 	for _, element := range s {
-		if predicate(element) {
-			output = append(output, element)
-		}
+		wg.Add(1)
+		go func(e T) {
+			// Освобождаем горутину после завершения работы
+			defer wg.Done()
+			// Если predicate вернет true, то добавляем элемент в result
+			if predicate(e) {
+				mux.Lock() // Блокировка мьютекса для записи
+				result = append(result, e)
+				mux.Unlock() // Разблокировка мьютекса
+			}
+		}(element)
 	}
-	return output
+	wg.Wait() // Ожидание завершения всех горутин
+	return result
 }
